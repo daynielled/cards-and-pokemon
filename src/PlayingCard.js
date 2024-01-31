48f6331c325e22779934148e0d029ff0201ea618
DD <ddonegal1@gmail.com>
2024-01-31 12:41:30 -0500

    utilized hook function in the 2 card components

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import backOfCard from "./back.png";
 import "./PlayingCard.css"
+import useFlip from "./hook";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flipIsFacingUp] = useFlip(true);
+ 
   return (
     <img
       src={isFacingUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={flipIsFacingUp}
       className="PlayingCard Card"
     />
   );
