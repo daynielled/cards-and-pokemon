07b88208a62aba7b6165bd4fa1bc8e0567794e09
DD <ddonegal1@gmail.com>
2024-01-31 13:38:25 -0500

    added a useAxios hook

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,28 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import {v1 as uuid} from "uuid";
-import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import useAxios from "./hooks/useAxios";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  const [data, fetchData, error, setUrl] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/")
+  
+  if(error) {
+    return <div>Error: {error.message}</div>
+  }
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={() => setUrl("https://deckofcardsapi.com/api/deck/new/draw/")}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        {data.map(cardData => (
+          <PlayingCard key={uuid()} front={cardData.cards[0].image} />
         ))}
       </div>
     </div>
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,11 +1,31 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
-const useAxios = (url = {}) => {
-    const [response, setResponse] = useState(null);
+const useAxios = (initialUrl) => {
+    const [url, setUrl] = useState(initialUrl);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [isFetching, setIsfetching] = useState(false);
 
-    useEffect
+    const fetchData = async () => {
+         try{
+                const res = await axios.get(url);
+                setData((prevData) => [...prevData, res.data]);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsfetching(false); //set fetching status to false
+            }
+        };
+
+        useEffect(() => {
+           if(isFetching) {
+            fetchData();
+           } 
+        }, [url, isFetching]); //Only fetch data when is fetching
+
+        return [data,() => setIsfetching(true),error, setUrl];
+   
 }
 
 export default useAxios;
-
-
